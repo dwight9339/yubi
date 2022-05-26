d2b4cf740e0d63494a2dfc59010562b8bdd3dd49
David White <whited9339@gmail.com>
2022-05-26 12:23:41 -0600

    Restructured parent files and implemented route propagation

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,99 +1,30 @@
-import {
-  ApolloClient,
-  ApolloProvider,
-  HttpLink,
-  InMemoryCache,
-} from "@apollo/client";
-import {
-  Provider as AppBridgeProvider,
-  useAppBridge,
-} from "@shopify/app-bridge-react";
-import { authenticatedFetch } from "@shopify/app-bridge-utils";
-import { Redirect } from "@shopify/app-bridge/actions";
-import { AppProvider as PolarisProvider } from "@shopify/polaris";
-import translations from "@shopify/polaris/locales/en.json";
-import "@shopify/polaris/build/esm/styles.css";
 import { Outlet } from "react-router-dom";
 import { Page, Stack} from "@shopify/polaris"
+import { useLocation } from "react-router-dom";
+import { useRoutePropagation } from "@shopify/app-bridge-react";
 import useWindowDimensions from "../utils/hooks/useWindowDimensions";
 
 export default function App() {
   const { width: windowWidth } = useWindowDimensions();
+  const location = useLocation();
+
+  useRoutePropagation(location);
   
   return (
-    <PolarisProvider i18n={translations}>
-      <AppBridgeProvider
-        config={{
-          apiKey: process.env.SHOPIFY_API_KEY,
-          host: new URL(location).searchParams.get("host"),
-          forceRedirect: true,
-        }}
+    <Page 
+      fullWidth
+    >
+      <Stack 
+        distribution="center"
       >
-        <MyProvider>
-        <Page 
-          fullWidth
+        <div
+          style={{
+            width: windowWidth * 0.8
+          }}
         >
-          <Stack 
-            distribution="center"
-          >
-            <div
-              style={{
-                width: windowWidth * 0.8
-              }}
-            >
-              <Outlet />
-            </div>
-          </Stack>
-        </Page>
-        </MyProvider>
-      </AppBridgeProvider>
-    </PolarisProvider>
+          <Outlet />
+        </div>
+      </Stack>
+    </Page>
   );
-}
-
-function MyProvider({ children }) {
-  const app = useAppBridge();
-  const cacheOptions = {
-    typePolicies: {
-      Query: {
-        fields: {
-          products: {
-            keyArgs: false
-          }
-        }
-      }
-    }
-  }
-
-  const client = new ApolloClient({
-    cache: new InMemoryCache(cacheOptions),
-    link: new HttpLink({
-      credentials: "include",
-      fetch: userLoggedInFetch(app),
-    }),
-  });
-
-  return <ApolloProvider client={client}>{children}</ApolloProvider>;
-}
-
-export function userLoggedInFetch(app) {
-  const fetchFunction = authenticatedFetch(app);
-
-  return async (uri, options) => {
-    const response = await fetchFunction(uri, options);
-
-    if (
-      response.headers.get("X-Shopify-API-Request-Failure-Reauthorize") === "1"
-    ) {
-      const authUrlHeader = response.headers.get(
-        "X-Shopify-API-Request-Failure-Reauthorize-Url"
-      );
-
-      const redirect = Redirect.create(app);
-      redirect.dispatch(Redirect.Action.APP, authUrlHeader || `/auth`);
-      return null;
-    }
-
-    return response;
-  };
-}
+};
--- src/app/entry-client.jsx
+++ src/app/entry-client.jsx
@@ -1,3 +1,18 @@
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import {
+  Provider as AppBridgeProvider,
+  useAppBridge
+} from "@shopify/app-bridge-react";
+import { authenticatedFetch } from "@shopify/app-bridge-utils";
+import { Redirect } from "@shopify/app-bridge/actions";
+import { AppProvider as PolarisProvider } from "@shopify/polaris";
+import translations from "@shopify/polaris/locales/en.json";
+import "@shopify/polaris/build/esm/styles.css";
 import ReactDOM from "react-dom";
 import {
   BrowserRouter,
@@ -9,21 +24,82 @@ import App from "./App";
 
 import { ProductsListPage } from "../components/productsList/ProductsListPage";
 import { ProductPage } from "../components/productPage/ProductPage";
-import { VariantPage } from "../components/variantViewEdit/VariantPage";
+import { VariantPage } from "../components/variantPage/VariantPage";
+
+function MyProvider({ children }) {
+  const app = useAppBridge();
+  const cacheOptions = {
+    typePolicies: {
+      Query: {
+        fields: {
+          products: {
+            keyArgs: false
+          }
+        }
+      }
+    }
+  }
+
+  const client = new ApolloClient({
+    cache: new InMemoryCache(cacheOptions),
+    link: new HttpLink({
+      credentials: "include",
+      fetch: userLoggedInFetch(app),
+    }),
+  });
+
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+}
+
+export function userLoggedInFetch(app) {
+  const fetchFunction = authenticatedFetch(app);
+
+  return async (uri, options) => {
+    const response = await fetchFunction(uri, options);
+
+    if (
+      response.headers.get("X-Shopify-API-Request-Failure-Reauthorize") === "1"
+    ) {
+      const authUrlHeader = response.headers.get(
+        "X-Shopify-API-Request-Failure-Reauthorize-Url"
+      );
+
+      const redirect = Redirect.create(app);
+      redirect.dispatch(Redirect.Action.APP, authUrlHeader || `/auth`);
+      return null;
+    }
+
+    return response;
+  };
+}
 
 ReactDOM.render((
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route index element={<Navigate to="/products" />} />
-        <Route path="products" element={<ProductsListPage />} />
-        <Route path="product">
-          <Route path=":productId" element={<ProductPage />} />
-        </Route>
-        <Route path="variant">
-          <Route path=":variantId" element={<VariantPage />} />
-        </Route>
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <PolarisProvider i18n={translations}>
+    <AppBridgeProvider
+      config={{
+        apiKey: process.env.SHOPIFY_API_KEY,
+        host: new URL(location).searchParams.get("host"),
+        forceRedirect: true,
+      }}
+    >
+      <MyProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />}>
+              <Route index element={<Navigate to="/products" />} />
+              <Route path="products" element={<ProductsListPage />} />
+              <Route path="product">
+                <Route path=":productId" element={<ProductPage />} />
+              </Route>
+              <Route path="variant">
+                <Route path=":variantId" element={<VariantPage />} />
+              </Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </MyProvider>
+    </AppBridgeProvider>
+  </PolarisProvider>
 ), document.getElementById("app"));
+
+
