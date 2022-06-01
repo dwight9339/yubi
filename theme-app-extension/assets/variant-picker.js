a0e283b607fb5b7756a49ed9dfc0fed118b9e991
David White <whited9339@gmail.com>
2022-06-01 12:34:07 -0600

    Implemented variant picker app block

---
+++ theme-app-extension/assets/variant-picker.js
@@ -0,0 +1,26 @@
+class variantPicker extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.thumbnails = this.querySelectorAll(".variant-thumbnail");
+    this.thumbnails.forEach((thumbnail) => {
+      thumbnail.addEventListener("click", this.setSelectedVariant);
+    });
+  }
+
+  disconnectedCallback() {
+    this.thumbnails.forEach((thumbnail) => {
+      thumbnail.removeEventListener("click", this.setSelectedVariant);
+    });
+  }
+
+  setSelectedVariant({ currentTarget }) {
+    const { variantId, variantImageId } = currentTarget.dataset;
+    const url = `${window.location.pathname}?variant=${variantId}`;
+    window.location.search = `variant=${variantId}`;
+  }
+}
+
+customElements.define("variant-picker", variantPicker);
