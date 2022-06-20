7e44e078777fe80a10305ef0729b3ca51ddb7693
David White <whited9339@gmail.com>
2022-06-20 09:35:40 -0600

    Added content and action

--- src/components/common/ReturningUserModal.jsx
+++ src/components/common/ReturningUserModal.jsx
@@ -6,7 +6,14 @@ export const ReturningUserModal = ({ show, onClose }) => {
       open={show}
       title="Welcome Back!"
       onClose={onClose}
-      message="Welcome back, user!"
+      message="Thank you for giving us another shot. Products that have
+      been updated may no longer be compatible and may need to be converted.
+      Any variants you wish to have deleted after purchase will need to be 
+      marked as such again."
+      primaryAction={{
+        content: "Ok",
+        onAction: onClose
+      }}
     />
   )
 }
