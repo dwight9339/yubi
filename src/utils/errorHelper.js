3fdd60f26b3b1446423c9422db6671caf9e0e071
David White <whited9339@gmail.com>
2022-06-21 10:40:46 -0600

    Added handleUserErrors helper method

--- src/utils/errorHelper.js
+++ src/utils/errorHelper.js
@@ -1,3 +1,14 @@
-export const extractUserErrors = (obj) => {
-  return obj.userErrors.map((error) => error.message);
+export const handleUserErrors = (data) => {
+  const errors = [];
+  
+  Object.entries(data).forEach(([key, value]) => {
+    errors.push(...value.userErrors
+      .filter(({ message }) => !errors.includes(message))
+      .map(({ message }) => message)
+    );
+  });
+
+  if (errors.length) {
+    throw errors;
+  }
 }
