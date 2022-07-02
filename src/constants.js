6bcd978b15b1ea146e156e77be54edee1fc186de
David White <whited9339@gmail.com>
2022-07-01 19:45:51 -0600

    Replaced emailjs logins with empty strings

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ export const METAFIELD_NAMESPACE = {
   general: "uvapp",
   variants: "uvapp-variants",
   products: "uvapp-products",
-  installed: "uvapp-installed"
+  installed: "uvapp-installed",
 };
 
 // graphql queries will add/delete/update data under these keys in the
@@ -12,7 +12,7 @@ export const METAFIELD_KEY = {
   variantName: "variant_name",
   variantDescription: "variant_description",
   isUniqueVariant: "is_uv",
-  deleteAfterPurchase: "delete_after_purchase"
+  deleteAfterPurchase: "delete_after_purchase",
 };
 
 export const ROUTES = {
@@ -22,17 +22,17 @@ export const ROUTES = {
 export const QUERY_PAGE_SIZE = {
   products: 5,
   variants: 5,
-  variantsPreview: 3
+  variantsPreview: 3,
 };
 
 export const UV_TAG = "unique variants";
 export const UV_TEMPLATE_SUFFIX = "unique-variants";
 export const IMAGE_SIZE_LIMIT = 20.971e6; // In bytes
-export const SUPPORTED_IMAGE_TYPES = ["jpeg", "png", "gif"]
-export const GENERIC_ERROR_TEXT = "We've encountered an error. Please try again. If the problem persists, please file a support ticket."
-
+export const SUPPORTED_IMAGE_TYPES = ["jpeg", "png", "gif"];
+export const GENERIC_ERROR_TEXT =
+  "We've encountered an error. Please try again. If the problem persists, please file a support ticket.";
 
 // EmailJS
-export const EMAILJS_SERVICE_ID = <INSERT SERVICE ID>
-export const EMAILJS_TEMPLATE_ID = <INSERT TEMPLATE ID>
-export const EMAILJS_USER_ID = <INSERT API PUBLIC KEY>
+export const EMAILJS_SERVICE_ID = "";
+export const EMAILJS_TEMPLATE_ID = "";
+export const EMAILJS_USER_ID = "";
