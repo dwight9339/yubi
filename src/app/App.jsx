76ca6b61449023c1ced18f388d90e831b28ca973
David White <whited9339@gmail.com>
2022-07-02 15:47:59 -0600

    Removed references to returning user modal

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,51 +1,37 @@
 import { Outlet, useLocation } from "react-router-dom";
-import { 
-  Stack
-} from "@shopify/polaris"
+import { Stack } from "@shopify/polaris";
 import { useRoutePropagation } from "@shopify/app-bridge-react";
 import { useEffect, useState } from "react";
 import { AppFrame } from "./AppFrame";
 import { NewUserModal } from "../components/modals/NewUserModal";
-import { ReturningUserModal } from "../components/modals/ReturningUserModal";
 
 export const App = () => {
   const location = useLocation();
   const urlParams = new URLSearchParams(location.search);
 
   const [showNewUserModal, setShowNewUserModal] = useState(false);
-  const [showReturningUserModal, setShowReturningUserModal] = useState(false);
 
   useEffect(() => {
     const newUser = urlParams.get("newUser");
-    const returningUser = urlParams.get("returningUser");
 
     if (newUser) {
       setShowNewUserModal(true);
     }
-    if (returningUser) {
-      setShowReturningUserModal(true);
-    }
   }, []);
 
   useRoutePropagation(location);
-  
+
   return (
-    <Stack 
-      distribution="center"
-    >
+    <Stack distribution="center">
       <div
-        style={{ 
-          width: "100%"
+        style={{
+          width: "100%",
         }}
       >
         <AppFrame>
-          <NewUserModal 
-            show={showNewUserModal} 
+          <NewUserModal
+            show={showNewUserModal}
             onClose={() => setShowNewUserModal(false)}
-          />
-          <ReturningUserModal 
-            show={showReturningUserModal}
-            onClose={() => setShowReturningUserModal(false)}
           />
           <Outlet />
         </AppFrame>
